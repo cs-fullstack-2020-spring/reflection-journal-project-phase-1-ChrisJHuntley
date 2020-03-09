32ddd82d64e3f3ecfd94f13c0ce02b68ad69e742
chris huntley <cjhuntley4@gmail.com>
2020-03-08 21:48:42 -0500

    I figured it out

--- index.js
+++ index.js
@@ -1,73 +1,44 @@
-// pulling my elements and testing
-let entry = document.querySelector('.entry')
-console.log(entry)
-let date = document.querySelector('.date')
-console.log(date)
-let confidence = document.querySelector('.confidence')
-console.log(confidence)
-let save = document.querySelector('#save')
-console.log(save)
-
-
-submitForm = (event) => {
-    event.preventDefault(); 
-    let userInput = {
-        creation_date: date.nodeValue,
-        journal_entry: entry,
-        confidence_level: confidence
-    }
-    console.log(` creation_date:: ${date}
-     journal_entry: ${entry}
-     confidence_level:  : ${confidence}`)
-}
-// created object literal notation
-
-let input = {
-    creation_date: date.value,
-    journal_entry: entry.value,
-    confidence_level: confidence.value
-}
  
-// Created array to store object literal notation
-const journalArray = [
-    {
-        creationDate: `${input.creation_date}`,
-        entry: `${input.journal_entry}`,
-        level: `${input.confidence_level}`
-    }
-    
-
-]
-console.log(journalArray)
-//  set an event listener to
-// date.addEventListener('click', thedate)
-function thedate(enter){
-    date.innerHTML= date
-
-}
-// entry.addEventListener('click', test)
-function test(enter){
-    entry.innerHTML= entry
-}
-
+let confidence = document.querySelector("#confidence");
+// console.log(confidence);
+let entry = document.querySelector("#entry");
+// console.log(entry);
+let date = document.querySelector("#date");
+// console.log(date);
+let submit = document.querySelector("#submit");
+// console.log(submit);
+let entryBlock = document.querySelector("#entryBlock")
  
-save.addEventListener('click', evt)
-function evt(event) {
-        console.log(journalArray)
-        
+// define and empty array to keep journal entries
+let entryArray = [];
+
+// create a callback function for on click event
+let formSubmission = (event) => {
+    event.preventDefault(); // keep form from reloading
+    // console.log("callback works"); // check that call back is being called
+
+    // define and object using object literal notation with form input values as property values
+    let newEntry = {
+        confidence : confidence.value,
+        entry : entry.value,
+        date : date.value
     }
-//     else {
-        
-//     }
-// }
-// function completed(){
-// let check=confrim('Would you like to retry ')
-//     if (check==true) {
-//     }
-// }
-// console.log(completed());
-// //     function howmany(doit){
-// for(i=0;i<journalArray.length; i++){
-//   let doit= input.push(journalArray)
-// }
-//     }
+    // console.log(newEntry) // check that object is being created
+    entryArray.push(newEntry); // push object to array defined on line 16
+    displayEntries(entryArray); // call function to display entries and pass array
+}
+
+// arrow function to display entries from array
+let displayEntries = (entryArray) => {
+    // console.log("array function called"); // check that function is being called
+    let entryBlockString = ""; // define an empty string to build string to display
+    entryArray.forEach(entry => {
+        entryBlockString = `${entryBlockString} ${entry.entry} \n ${entry.confidence} \n ${entry.date}\n`
+        // console.log(entryBlockString);
+        // iterate through array and for each element in the array append the new element to the string defined on line 37
+    });
+    entryBlock.innerText = `${entryBlockString}`; // display built string as innerText in empty div
+}
+
+// call callback function when form submit button is clicked
+submit.addEventListener("click", formSubmission);
