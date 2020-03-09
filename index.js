5140404c9389d0ecf942ae4217dc4f552875dad8
chris huntley <cjhuntley4@gmail.com>
2020-03-08 22:12:51 -0500

    Okay done foreal now

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  
 let confidence = document.querySelector("#confidence");
 // console.log(confidence)
@@ -15,20 +14,6 @@ let entryArray = [];
 
 // create a function for event
 let formSubmission = (event) => {
-=======
-// pulling my elements and testing
-let entry = document.querySelector('.entry')
-console.log(entry)
-let date = document.querySelector('.date')
-console.log(date)
-let confidence = document.querySelector('.confidence')
-console.log(confidence)
-let save = document.querySelector('#save')
-console.log(save)
-
-// !! looks like you should be calling this function in your event handler
-submitForm = (event) => {
->>>>>>> 519cd7e10b84e04cff51b7c9597ca714f42c1eb5
     event.preventDefault(); 
     // keep form from reloading
     
@@ -46,7 +31,6 @@ submitForm = (event) => {
     displayEntries(entryArray);   
 }
 
-<<<<<<< HEAD
 // show entries to array
 let displayEntries = (entryArray) => {
   
@@ -55,22 +39,6 @@ let displayEntries = (entryArray) => {
     entryArray.forEach(entry => {
         entryBlockString = `${entryBlockString} ${entry.entry} \n ${entry.confidence} \n ${entry.date}\n`
         // console.log(entryBlockString)
-=======
-//!! this should happen inside of your callback function
-let input = {
-    creation_date: date.value,
-    journal_entry: entry.value,
-    confidence_level: confidence.value
-}
- // !! why is this array populated with random data not submitted by the user?? (don't just copy the examples from the README)
-// Created array to store object literal notation
-const journalArray = [
-    {
-        creationDate: `${input.creation_date}`,
-        entry: `${input.journal_entry}`,
-        level: `${input.confidence_level}`
-    }
->>>>>>> 519cd7e10b84e04cff51b7c9597ca714f42c1eb5
     
     });
 // display entries in browser
